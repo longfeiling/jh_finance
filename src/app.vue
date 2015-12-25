3e3f609e4a5e9f751a077c70b468ba1fa3593bf3
Feeling <longfeiling@126.com>
2015-12-25 10:51:49 +0800

    test

---
+++ src/app.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+		<h1>Hello,app</h1>
+		<p>
+			<a v-link="path : '/foo">Go to foo</a>
+			<a v-link="path : '/bar">Go to Bar</a>
+		</p>
+		<!-- 路由外链 -->
+	<rooter-view></rooter-view>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+// 定义组件
+var Foo = Vue.extend({
+	template:'<p>This is foo !</p>'
+})
+
+var Bar = Vue.extend({
+	template:'<p>This is Bar !</p>'
+})
+
+// 路由器需要一个根组件
+var app = Vue.extend({})
+
+// 创建一个路由实例
+var router = new VueRouter()
+
+// 定义路由规则，映射到一个组件上去  组件也可以使用 vue.extend({})
+router.map({
+	'/foo' : {
+		compontent : Foo
+	},
+	'/bar' : {
+		compontent : Bar
+	}
+})
+</script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Comp from './app.vue'
+
+new Vue({
+	el : 'body',
+	components : {
+		app1 : Comp
+	}
+})
